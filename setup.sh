c7786ce47f1b68e345894d4ba746c66ada5bb8eb
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-08-19 14:40:49 -0400

    Update and rename setup2.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate base
+sudo apt install openssh-server -y
+sudo apt install tmux -y
 
 # Clone the repository
 git clone https://github.com/farshadghodsian/llava-amd-radeon-demo
 
 # Change directory to the cloned repository
 cd llava-amd-radeon-demo
-
-# Create a conda environment named llava by cloning base
-conda create --name llava --clone base -y
-
-# Activate the llava environment
-conda activate llava
-
-# Upgrade pip (optional, but recommended)
-pip install --upgrade pip
 
 # Install required packages
 pip install accelerate fastchat gradio transformers==4.37.2
