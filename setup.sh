8161bcea98646a48e843cb44f081f3e0ba2e236d
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-08-19 15:18:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ git clone https://github.com/farshadghodsian/llava-amd-radeon-demo
 cd llava-amd-radeon-demo
 
 # Install required packages
-pip install accelerate fastchat gradio transformers==4.37.2
-
+pip3 install accelerate fastchat gradio transformers==4.37.2
+pip3 install SentencePiece
 echo "Setup complete. You can now start via sh ./start.sh"
