9c7d0919d8c60330a91950a06f0cb0c7582af107
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-08-20 14:34:00 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ tmux split-window -h -t $SESSION_NAME
 # Send commands to each pane
 tmux send-keys -t $SESSION_NAME:0.0 "export PYTHONPATH=~/src/AI-Demo/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/AI-Demo/llava-amd-radeon-demo/llava/serve/controller.py" C-m
 
-tmux send-keys -t $SESSION_NAME:0.1 "sleep 5 && export PYTHONPATH=~/src/AI-Demo/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/AI-Demo/llava-amd-radeon-demo/llava/serve/model_worker.py --model-path liuhaotian/llava-v1.6-34b" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "sleep 5 && export PYTHONPATH=~/src/AI-Demo/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/AI-Demo/llava-amd-radeon-demo/llava/serve/model_worker.py --model-path liuhaotian/llava-v1.6-mistral-7b" C-m
 
 tmux send-keys -t $SESSION_NAME:0.2 "sleep 40 && export PYTHONPATH=~/src/AI-Demo/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/AI-Demo/llava-amd-radeon-demo/llava/serve/gradio_web_server.py" C-m
 
