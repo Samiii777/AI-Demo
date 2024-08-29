f6ce3d94bbaa87645f7fca2edf830ac6d1583494
Samiii777 <samankhatir@gmail.com>
2024-08-29 10:31:53 -0400

    add inference for sd 2.1

--- sd-finetune/inference_lora_SD2.1.py
+++ sd-finetune/inference_lora_SD2.1.py
@@ -1,10 +1,10 @@
 from diffusers import DiffusionPipeline
 import torch
 
-model_path="lora_weights/sd1.5"
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+model_path="lora_weights/sd2.1"
+pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1")
 pipeline.unet.load_attn_procs(model_path)
 pipeline.to("cuda")
 image = pipeline("A cat in a bucket").images[0]
-image.save("SD1.5-cat.png")
+image.save("SD2.1-cat.png")
 image.show()
