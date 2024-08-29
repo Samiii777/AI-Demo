6a65c78aa1c663c2edc0be507b5803778501506c
Samiii777 <samankhatir@gmail.com>
2024-08-29 10:35:59 -0400

    fix

--- sd-finetune/inference_SD2.1.py
+++ sd-finetune/inference_SD2.1.py
@@ -1,9 +1,7 @@
 from diffusers import DiffusionPipeline
 import torch
 
-model_path="lora_weights/sd2.1"
 pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1")
-pipeline.unet.load_attn_procs(model_path)
 pipeline.to("cuda")
 image = pipeline("A cat in a bucket").images[0]
 image.save("SD2.1-cat.png")
