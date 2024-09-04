ec684e48a0bd4308e5306394e99ce03fa57f67ab
Samiii777 <samankhatir@gmail.com>
2024-09-04 12:00:14 -0400

    add venv

--- llama-finetune/download.sh
+++ llama-finetune/download.sh
@@ -1,1 +1,2 @@
+source /venv/bin/activate
 tune download meta-llama/Meta-Llama-3.1-8B-Instruct --output-dir /tmp/Meta-Llama-3.1-8B-Instruct --ignore-patterns "original/consolidated.00.pth" --hf-token 
